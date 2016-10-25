c4be0f15e5635a441ee50e75062d2adf07e2a4d1
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-10-25 08:45:41 +0200

    v.0.0.1

---
+++ app/main.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import _ from "underscore";
+
+const task1 = function () {
+  console.log("qq");
+};
+
+const task2 = function () {
+};
+
+const task3 = function () {
+};
+
+const onHeroesDataLoad = function (heroes) {
+};
+
+const onHeroesPageLoad = function () {
+};
+
+const task4 = function () {
+};
---
+++ systemjs.config.js
@@ -0,0 +1,34 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      'app': 'app',
+      'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
+      'systemjs-babel-build': 'npm:systemjs-plugin-babel/systemjs-babel-browser.js',
+      'text': 'npm:systemjs-plugin-text/text.js',
+
+      // bb bundles
+      'underscore': "npm:underscore/underscore.js",
+      'jquery': "npm:jquery/dist/jquery.js"
+    },
+
+    transpiler: 'plugin-babel',
+
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
